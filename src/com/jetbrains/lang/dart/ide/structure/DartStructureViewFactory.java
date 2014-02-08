2aa3ce25b66c498575e4ae538277c16db848e6e0
VISTALL <vistall.valeriy@gmail.com>
2014-02-08 23:37:15 +0200

    compilation fixed

--- src/com/jetbrains/lang/dart/ide/structure/DartStructureViewFactory.java
+++ src/com/jetbrains/lang/dart/ide/structure/DartStructureViewFactory.java
@@ -1,28 +1,35 @@
 package com.jetbrains.lang.dart.ide.structure;
 
+import org.jetbrains.annotations.NotNull;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
 
 /**
- * @author: Fedor.Korotkov
+ * @author Fedor.Korotkov
  */
-public class DartStructureViewFactory implements PsiStructureViewFactory {
-  @Override
-  public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-    return new TreeBasedStructureViewBuilder() {
-      @NotNull
-      public StructureViewModel createStructureViewModel() {
-        return new DartStructureViewModel(psiFile);
-      }
+public class DartStructureViewFactory implements PsiStructureViewFactory
+{
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
+			@Override
+			@NotNull
+			public StructureViewModel createStructureViewModel(Editor editor)
+			{
+				return new DartStructureViewModel(psiFile);
+			}
 
-      @Override
-      public boolean isRootNodeShown() {
-        return false;
-      }
-    };
-  }
+			@Override
+			public boolean isRootNodeShown()
+			{
+				return false;
+			}
+		};
+	}
 }
