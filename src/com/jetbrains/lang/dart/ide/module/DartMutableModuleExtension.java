5a8e0e6d665b07d72363be3329eb4612e4246ba2
VISTALL <vistall.valeriy@gmail.com>
2015-08-13 17:07:56 +0300

    compilation

--- src/com/jetbrains/lang/dart/ide/module/DartMutableModuleExtension.java
+++ src/com/jetbrains/lang/dart/ide/module/DartMutableModuleExtension.java
@@ -4,17 +4,20 @@ import javax.swing.JComponent;
 
 import org.consulo.module.extension.MutableModuleExtensionWithSdk;
 import org.consulo.module.extension.MutableModuleInheritableNamedPointer;
-import org.consulo.module.extension.ui.ModuleExtensionWithSdkPanel;
+import org.consulo.module.extension.ui.ModuleExtensionSdkBoxBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.RequiredDispatchThread;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.ModuleRootLayer;
+import com.intellij.util.ui.JBUI;
 
 /**
  * @author VISTALL
  * @since 17:50/08.06.13
  */
-public class DartMutableModuleExtension extends DartModuleExtension implements MutableModuleExtensionWithSdk<DartModuleExtension>
+public class DartMutableModuleExtension extends DartModuleExtension implements
+		MutableModuleExtensionWithSdk<DartModuleExtension>
 {
 	public DartMutableModuleExtension(@NotNull String id, @NotNull ModuleRootLayer module)
 	{
@@ -28,11 +31,13 @@ public class DartMutableModuleExtension extends DartModuleExtension implements M
 		return (MutableModuleInheritableNamedPointer<Sdk>) super.getInheritableSdk();
 	}
 
+	@RequiredDispatchThread
 	@Nullable
 	@Override
 	public JComponent createConfigurablePanel(@Nullable Runnable runnable)
 	{
-		return wrapToNorth(new ModuleExtensionWithSdkPanel(this, runnable));
+		return JBUI.Panels.verticalPanel().addComponent(ModuleExtensionSdkBoxBuilder.createAndDefine(this,
+				runnable).build());
 	}
 
 	@Override
