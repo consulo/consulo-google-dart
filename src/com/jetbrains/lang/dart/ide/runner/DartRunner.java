9e5d04540815167462477dcda8357fa872a138e1
VISTALL <vistall.valeriy@gmail.com>
2015-10-17 20:41:52 +0300

    compilation

--- src/com/jetbrains/lang/dart/ide/runner/DartRunner.java
+++ src/com/jetbrains/lang/dart/ide/runner/DartRunner.java
@@ -1,7 +1,6 @@
 package com.jetbrains.lang.dart.ide.runner;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.ExecutionResult;
 import com.intellij.execution.configurations.RunProfile;
@@ -14,7 +13,6 @@ import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.net.NetUtils;
 import com.intellij.xdebugger.XDebugProcess;
@@ -40,25 +38,23 @@ public class DartRunner extends DefaultProgramRunner
 	@Override
 	public boolean canRun(final @NotNull String executorId, final @NotNull RunProfile profile)
 	{
-		return profile instanceof DartRunConfigurationBase && (DefaultRunExecutor.EXECUTOR_ID.equals(executorId) || DefaultDebugExecutor.EXECUTOR_ID
-				.equals(executorId));
+		return profile instanceof DartRunConfigurationBase && (DefaultRunExecutor.EXECUTOR_ID.equals(executorId) || DefaultDebugExecutor.EXECUTOR_ID.equals(executorId));
 	}
 
-	protected RunContentDescriptor doExecute(final @NotNull Project project, final @NotNull RunProfileState state,
-			final @Nullable RunContentDescriptor contentToReuse, final @NotNull ExecutionEnvironment env) throws ExecutionException
+	protected RunContentDescriptor doExecute(final @NotNull RunProfileState state, final @NotNull ExecutionEnvironment env) throws ExecutionException
 	{
 		final String executorId = env.getExecutor().getId();
 
 		if(DefaultRunExecutor.EXECUTOR_ID.equals(executorId))
 		{
-			return super.doExecute(project, state, contentToReuse, env);
+			return super.doExecute(state, env);
 		}
 
 		if(DefaultDebugExecutor.EXECUTOR_ID.equals(executorId))
 		{
 			try
 			{
-				return doExecuteDartDebug(state, contentToReuse, env);
+				return doExecuteDartDebug(state, env);
 			}
 			catch(RuntimeConfigurationError e)
 			{
@@ -70,8 +66,7 @@ public class DartRunner extends DefaultProgramRunner
 		return null;
 	}
 
-	private RunContentDescriptor doExecuteDartDebug(final @NotNull RunProfileState state, final @Nullable RunContentDescriptor contentToReuse,
-			final @NotNull ExecutionEnvironment env) throws RuntimeConfigurationError, ExecutionException
+	private RunContentDescriptor doExecuteDartDebug(final @NotNull RunProfileState state, final @NotNull ExecutionEnvironment env) throws RuntimeConfigurationError, ExecutionException
 	{
 		FileDocumentManager.getInstance().saveAllDocuments();
 
