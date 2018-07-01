053242e332465e4605ab695918eb721997a59815
Valery Semenchuk <vistall.valeriy@gmail.com>
2018-07-01 19:04:10 +0300

    compilation

--- dart-impl/src/main/java/com/jetbrains/lang/dart/sdk/listPackageDirs/DartListPackageDirsLibraryType.java
+++ dart-impl/src/main/java/com/jetbrains/lang/dart/sdk/listPackageDirs/DartListPackageDirsLibraryType.java
@@ -1,10 +1,9 @@
 package com.jetbrains.lang.dart.sdk.listPackageDirs;
 
 import javax.annotation.Nonnull;
-import javax.swing.Icon;
+import javax.annotation.Nullable;
 import javax.swing.JComponent;
 
-import javax.annotation.Nullable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.libraries.LibraryType;
 import com.intellij.openapi.roots.libraries.NewLibraryConfiguration;
@@ -12,14 +11,13 @@ import com.intellij.openapi.roots.libraries.PersistentLibraryKind;
 import com.intellij.openapi.roots.libraries.ui.LibraryEditorComponent;
 import com.intellij.openapi.roots.libraries.ui.LibraryPropertiesEditor;
 import com.intellij.openapi.vfs.VirtualFile;
-import consulo.awt.TargetAWT;
+import consulo.ui.image.Image;
 import icons.DartIcons;
 
 public class DartListPackageDirsLibraryType extends LibraryType<DartListPackageDirsLibraryProperties>
 {
 
-	public static final PersistentLibraryKind<DartListPackageDirsLibraryProperties> LIBRARY_KIND = new
-			PersistentLibraryKind<DartListPackageDirsLibraryProperties>("DartListPackageDirsLibraryType")
+	public static final PersistentLibraryKind<DartListPackageDirsLibraryProperties> LIBRARY_KIND = new PersistentLibraryKind<DartListPackageDirsLibraryProperties>("DartListPackageDirsLibraryType")
 	{
 		@Override
 		@Nonnull
@@ -43,24 +41,22 @@ public class DartListPackageDirsLibraryType extends LibraryType<DartListPackageD
 
 	@Override
 	@Nullable
-	public NewLibraryConfiguration createNewLibrary(@Nonnull final JComponent parentComponent, @Nullable final VirtualFile contextDirectory,
-			@Nonnull final Project project)
+	public NewLibraryConfiguration createNewLibrary(@Nonnull final JComponent parentComponent, @Nullable final VirtualFile contextDirectory, @Nonnull final Project project)
 	{
 		return null;
 	}
 
 	@Override
 	@Nullable
-	public LibraryPropertiesEditor createPropertiesEditor(@Nonnull final LibraryEditorComponent<DartListPackageDirsLibraryProperties>
-			editorComponent)
+	public LibraryPropertiesEditor createPropertiesEditor(@Nonnull final LibraryEditorComponent<DartListPackageDirsLibraryProperties> editorComponent)
 	{
 		return null;
 	}
 
 	@Override
 	@Nullable
-	public Icon getIcon()
+	public Image getIcon()
 	{
-		return TargetAWT.to(DartIcons.Dart);
+		return DartIcons.Dart;
 	}
 }
