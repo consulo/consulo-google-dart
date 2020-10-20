b0d3d506a0bf92147e617473f3b8091ec83c251a
VISTALL <vistall.valeriy@gmail.com>
2020-10-20 21:24:38 +0300

    jakarta

--- dart-impl/src/main/java/com/jetbrains/lang/dart/DartProjectComponent.java
+++ dart-impl/src/main/java/com/jetbrains/lang/dart/DartProjectComponent.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import javax.inject.Singleton;
+import jakarta.inject.Singleton;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
