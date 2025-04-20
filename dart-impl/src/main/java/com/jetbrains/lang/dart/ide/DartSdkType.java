bf50c5135fcc5a6b10163e14d1f3ac4b6165fd95
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-20 16:46:26 +0400

    drop usage of EP_NAME

--- dart-impl/src/main/java/com/jetbrains/lang/dart/ide/DartSdkType.java
+++ dart-impl/src/main/java/com/jetbrains/lang/dart/ide/DartSdkType.java
@@ -1,8 +1,8 @@
 package com.jetbrains.lang.dart.ide;
 
-import com.jetbrains.lang.dart.DartIcons;
 import com.jetbrains.lang.dart.util.DartSdkUtil;
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.content.OrderRootType;
 import consulo.content.base.BinariesOrderRootType;
 import consulo.content.base.DocumentationOrderRootType;
@@ -10,8 +10,8 @@ import consulo.content.base.SourcesOrderRootType;
 import consulo.content.bundle.Sdk;
 import consulo.content.bundle.SdkModificator;
 import consulo.content.bundle.SdkType;
+import consulo.google.dart.icon.DartIconGroup;
 import consulo.ui.image.Image;
-
 import jakarta.annotation.Nonnull;
 
 /**
@@ -19,52 +19,53 @@ import jakarta.annotation.Nonnull;
  */
 @ExtensionImpl
 public class DartSdkType extends SdkType {
-  public static DartSdkType getInstance() {
-    return SdkType.EP_NAME.findExtension(DartSdkType.class);
-  }
+    public static DartSdkType getInstance() {
+        return Application.get().getExtensionPoint(SdkType.class).findExtension(DartSdkType.class);
+    }
 
-  public DartSdkType() {
-    super("Dart SDK");
-  }
+    public DartSdkType() {
+        super("Dart SDK");
+    }
 
-  @Override
-  public Image getIcon() {
-    return DartIcons.Dart;
-  }
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return DartIconGroup.dart();
+    }
 
-  @Nonnull
-  @Override
-  public String getPresentableName() {
-    return "Dart";
-  }
+    @Nonnull
+    @Override
+    public String getPresentableName() {
+        return "Dart";
+    }
 
-  @Override
-  public String suggestSdkName(String currentSdkName, String sdkHome) {
-    return "Dart " + getVersionString(sdkHome);
-  }
+    @Override
+    public String suggestSdkName(String currentSdkName, String sdkHome) {
+        return "Dart " + getVersionString(sdkHome);
+    }
 
-  @Override
-  public String getVersionString(String sdkHome) {
-    return DartSdkUtil.getSdkVersion(sdkHome);
-  }
+    @Override
+    public String getVersionString(String sdkHome) {
+        return DartSdkUtil.getSdkVersion(sdkHome);
+    }
 
-  @Override
-  public boolean isValidSdkHome(String path) {
-    return DartSdkUtil.getCompilerPathByFolderPath(path) != null;
-  }
+    @Override
+    public boolean isValidSdkHome(String path) {
+        return DartSdkUtil.getCompilerPathByFolderPath(path) != null;
+    }
 
-  @Override
-  public boolean isRootTypeApplicable(OrderRootType type) {
-    return type == BinariesOrderRootType.getInstance() || type == SourcesOrderRootType.getInstance() || type == DocumentationOrderRootType
-      .getInstance();
-  }
+    @Override
+    public boolean isRootTypeApplicable(OrderRootType type) {
+        return type == BinariesOrderRootType.getInstance() || type == SourcesOrderRootType.getInstance() || type == DocumentationOrderRootType
+            .getInstance();
+    }
 
-  @Override
-  public void setupSdkPaths(Sdk sdk) {
-    final SdkModificator modificator = sdk.getSdkModificator();
+    @Override
+    public void setupSdkPaths(Sdk sdk) {
+        final SdkModificator modificator = sdk.getSdkModificator();
 
-    DartSdkUtil.setupSdkPaths(sdk.getHomeDirectory(), modificator);
+        DartSdkUtil.setupSdkPaths(sdk.getHomeDirectory(), modificator);
 
-    modificator.commitChanges();
-  }
+        modificator.commitChanges();
+    }
 }
