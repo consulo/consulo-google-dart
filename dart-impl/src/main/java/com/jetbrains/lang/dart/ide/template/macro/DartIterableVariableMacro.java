b0ffe2131956894022e82628e8583d09020661a4
UNV <nikolay@yurchenko.su>
2026-04-21 11:01:41 +0300

    Fixing compilation after platform update.

--- dart-impl/src/main/java/com/jetbrains/lang/dart/ide/template/macro/DartIterableVariableMacro.java
+++ dart-impl/src/main/java/com/jetbrains/lang/dart/ide/template/macro/DartIterableVariableMacro.java
@@ -3,25 +3,26 @@ package com.jetbrains.lang.dart.ide.template.macro;
 import com.jetbrains.lang.dart.psi.DartClass;
 import consulo.annotation.component.ExtensionImpl;
 
+import consulo.localize.LocalizeValue;
 import jakarta.annotation.Nonnull;
 
 /**
- * @author: Fedor.Korotkov
+ * @author Fedor.Korotkov
  */
 @ExtensionImpl
 public class DartIterableVariableMacro extends DartFilterByClassMacro {
-  @Override
-  public String getName() {
-    return "dartIterableVariable";
-  }
+    @Override
+    public String getName() {
+        return "dartIterableVariable";
+    }
 
-  @Override
-  public String getPresentableName() {
-    return "dartIterableVariable()";
-  }
+    @Override
+    public LocalizeValue getPresentableName() {
+        return LocalizeValue.of("dartIterableVariable()");
+    }
 
-  @Override
-  protected boolean filter(@Nonnull DartClass dartClass) {
-    return dartClass.findMemberByName("iterator") != null;
-  }
+    @Override
+    protected boolean filter(@Nonnull DartClass dartClass) {
+        return dartClass.findMemberByName("iterator") != null;
+    }
 }
